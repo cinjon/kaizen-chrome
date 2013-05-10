628df22dcabb216953a4770863b07862a14c144f
terraformer <cinjon@alum.mit.edu>
2013-05-10 17:25:15 -0400

    squiggle search fixes, not complete, so on dev

--- chrome/squiggle.js
+++ chrome/squiggle.js
@@ -1,5 +1,5 @@
 bgp = chrome.extension.getBackgroundPage();
-query_url = bgp.domainName + '/query_maps';
+query_url = bgp.domainName + '/query/maps';
 
 document.addEventListener('DOMContentLoaded', function() {
     document.querySelector('#cancel').addEventListener('click', closeWindow);
@@ -9,12 +9,18 @@ document.addEventListener('DOMContentLoaded', function() {
 $("#search").keyup(function(e) {
     var query = $("#search").val();
     console.log("query is: " + query);
+    console.log("query_url is : " + query_url);
     $.getJSON(query_url,
               {'query':query},
               function(data) {
                   console.log('data is ' + data);
-              }
-             );
+                  $("#results").empty();
+                  $("#results").append("results are");
+                  for (var key in data) {
+                      console.log('key: ' + key + ', data: ' + data[key]);
+                      $("#results").append("<div>" + key + ":" + data[key] + "</div>");
+                  }
+              });
 });
 
 $(document).keyup(function(e) {
